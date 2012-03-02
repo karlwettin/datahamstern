02dacbc1264e71caf67340151b304c3189d16fce
kalle <karl.wettin@gmail.com>
2012-03-02 04:30:00 +0100

    starts the server

---
+++ maven/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+export MAVEN_OPTS="-d32 -Xms1G -Xmx4G -server"
+
+mkdir logs >> /dev/null
+cd server
+
+cp src/main/resources/datahamstern.properties.server src/main/resources/datahamstern.properties
+
+# Setup the ctl-c handler
+RETRY=1
+handle_ctl_c()
+{
+        RETRY=0
+}
+trap "handle_ctl_c" 2
+
+while [ "$RETRY" == "1" ]; do
+  export currentLog="`date +"%s"`.log"
+#  echo > ../logs/$currentLog
+#  rm ../current.log
+#  ln -s ../logs/$currentLog ../current.log
+  mvn -Djetty.port=8081 -Ddatahamstern.homePath=../ jetty:run >> ../logs/$currentLog
+done
